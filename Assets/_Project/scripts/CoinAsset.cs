708468c808640da41633f4f54c92bcf864f2b584
JottaP <joaopaulo_oli@hotmail.com>
2016-10-21 10:52:46 -0200

    S207

---
+++ Assets/_Project/scripts/CoinAsset.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Soomla.Store;
+using Soomla.Singletons;
+using System;
+
+public class CoinAsset : IStoreAssets
+{
+    public const string COINS_CURRENCY_ITEM_ID = "currency_coins";
+
+    public const string COINS_100_ID = "xxx SITE GOOGLA PLAY xxx";
+    public const string COINS_350_ID = "xxx SITE GOOGLA PLAY xxx";
+
+    public int GetVersion()
+    {
+        return 0;
+    }
+
+    public VirtualCurrency COIN_CURRENCY = new VirtualCurrency(
+        "Coins",
+        "coins to use the game",
+        COINS_CURRENCY_ITEM_ID);
+
+    public static VirtualCurrencyPack COINS_100_PACK = new VirtualCurrencyPack(
+        "100 coins",
+        "get 100 coins",
+        COINS_100_ID,
+        100,
+        COINS_100_ID,
+        new PurchaseWithMarket(COINS_100_ID, 0.99));
+
+
+    public static VirtualCurrencyPack COINS_350_PACK = new VirtualCurrencyPack(
+        "350 coins",
+        "get 350 coins",
+        COINS_350_ID,
+        350,
+        COINS_350_ID,
+        new PurchaseWithMarket(COINS_350_ID, 1.99));
+
+    public VirtualCurrencyPack[] GetCurrencyPacks()
+    {
+        return new VirtualCurrencyPack[] { COINS_100_PACK, COINS_350_PACK }; 
+    }
+
+    public VirtualCurrency[] GetCurrencies()
+    {
+        return new VirtualCurrency[] { COIN_CURRENCY };
+    }  
+
+    public VirtualGood[] GetGoods()
+    {
+        return new VirtualGood[] { };
+    }
+
+    public VirtualCategory[] GetCategories()
+    {
+        return new VirtualCategory[] { };
+    }
+    
+}
---
+++ Assets/_Project/scripts/CoinSystemManager.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Soomla;
+using Soomla.Store;
+
+public class CoinSystemManager : MonoBehaviour {
+
+    private int currentCoin = 0;
+    int temporaryCurrentCoinCount;
+    public float timeBetweenCoinIncrement = 0.05f;
+    public Text coinText;
+
+    public AudioSource audioSource;
+    public AudioClip clip;
+
+    bool userHasPlayerBefore = false;
+    public int initialCoinCount = 250;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>() as AudioSource;
+
+        userHasPlayerBefore = PlayerPrefs.GetString("userHasPlayerBefore").ToString().ToLowerInvariant() == "yes";
+        if (userHasPlayerBefore)
+        {
+            LoadCoins();
+        }
+        else
+        {
+            currentCoin = initialCoinCount;
+            SaveCoins();
+        }
+
+        coinText.text = currentCoin.ToString();
+        temporaryCurrentCoinCount = currentCoin;
+
+        StoreEvents.OnCurrencyBalanceChanged += OnCurrencyBalanceChanged;
+    }
+
+    void Update()
+    {
+        coinText.text = temporaryCurrentCoinCount.ToString();
+
+        if (temporaryCurrentCoinCount < currentCoin)
+        {
+            if (!IsInvoking("incrementeTemporaryCoins"))
+            {
+                coinText.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+                InvokeRepeating("incrementeTemporaryCoins", 0, timeBetweenCoinIncrement);
+            }
+        }
+    }
+
+    public void AddCoin(int quantity)
+    {
+        currentCoin += quantity;
+    }
+
+    void incrementeTemporaryCoins()
+    {
+        temporaryCurrentCoinCount++;
+        audioSource.PlayOneShot(clip);
+        if (temporaryCurrentCoinCount >= currentCoin)
+        {
+            CancelInvoke();
+            coinText.transform.localScale = new Vector3(1f, 1f, 1f);
+            SaveCoins();
+        }
+    }
+
+    void LoadCoins()
+    {
+        currentCoin = PlayerPrefs.GetInt("coins");
+    }
+
+    void SaveCoins()
+    {
+        PlayerPrefs.SetInt("coins", currentCoin);
+        userHasPlayerBefore = true;
+        PlayerPrefs.SetString("userHasPlayerBefore","yes");
+    }
+
+    public bool CanRemoveCoins(int quantity)
+    {
+        return (currentCoin - quantity > 0);
+    }
+
+    public void OnCurrencyBalanceChanged(VirtualCurrency virtualCurrency, int balance, int amountAdded )
+    {
+        currentCoin = balance;
+    }
+
+}
